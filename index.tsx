a7a4b5ef7acd2f91374ba0ef1ada397b4c9c9e49
Calum Mackervoy <c.mackervoy@gmail.com>
2021-09-02 20:13:02 +0200

    initial rollup config and package.json

---
+++ index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './pages/_app';
+import Home from './pages/index';
+
+ReactDOM.render(<Home></Home>, document.querySelector('#root'));
---
+++ rollup.config.js
@@ -0,0 +1,64 @@
+import typescript from "@rollup/plugin-typescript";
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import linaria from '@linaria/rollup';
+import css from 'rollup-plugin-css-only';
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+
+export default {
+  input: "./index.tsx",
+  plugins: [
+    linaria({ sourceMap: process.env.NODE_ENV !== 'production', }),
+    css({ output: 'styles.css', }),
+    nodeResolve({ 
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        module: true
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify( 'development' )
+    }),
+    babel({ presets: [
+      "@linaria",
+      ["@babel/preset-env", { targets: { node: "current" } }],
+      //["@babel/preset-react", {"runtime": "automatic"}],
+      "@babel/preset-react",
+      "@babel/preset-typescript",
+      ],
+      plugins: ["@babel/plugin-syntax-dynamic-import"],
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      exclude: /node_modules/
+    }),
+    commonjs({
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        include: /node_modules/
+    }),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    serve({
+        open: true,
+        verbose: true,
+        contentBase: ["", "public"],
+        host: "localhost",
+        port: 3000,
+    }),
+    livereload({ watch: "dist" }),
+  ],
+  external: [],
+  paths: {
+    "react": "https://unpkg.com/react@17/umd/react.development.js",
+    "react-dom": "https://unpkg.com/react-dom@17/umd/react-dom.development.js"
+  },
+  output: {
+    globals: {
+      'react': 'React',
+      'react-dom': 'ReactDOM',
+      'prop-types': 'PropTypes'
+    },
+    file: "dist/index.js",
+    //dir: 'dist',
+    format: "iife",
+    //sourcemap: true,
+  }
+};
