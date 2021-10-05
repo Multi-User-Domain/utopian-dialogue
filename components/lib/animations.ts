cdea29c9c64750e721138f4b214c7f6dcacc8305
Calum Mackervoy <c.mackervoy@gmail.com>
2021-10-05 14:37:47 +0200

    added new animations

--- components/lib/animations.ts
+++ components/lib/animations.ts
@@ -25,3 +25,29 @@ export const colourFadeAnimationCss = (fromColour: string, toColour: string, dur
                 }
             }`
 }
+
+export const fadeOutTransition = (durationSeconds: number) => {
+    return css`
+        visibility: hidden;
+        opacity: 0;
+        transition: visibility 0s ${durationSeconds}s, opacity ${durationSeconds}s linear;
+    `;
+}
+
+export const growTextAnimation = (fontSize: string, durationSeconds: number) => {
+    return css`
+        font-size: ${fontSize};
+        -webkit-animation-duration: ${durationSeconds}s;
+        -webkit-animation-name: growTextToScale;
+        animation-duration: ${durationSeconds}s;
+        animation-name: growTextToScale;
+        @keyframes createBox {
+            from {
+                transform: scale(0);
+            }
+            to {
+                transform: scale(1);
+            }
+        }
+    `;
+}
--- components/lib/constants.ts
+++ components/lib/constants.ts
@@ -1,4 +1,4 @@
 export const LONG_PAUSE = 2000;
 export const SHORT_PAUSE = 1000;
 export const SLOW_PACE = 100;
-export const FAST_PACE = 1000;
+export const FAST_PACE = 10;
