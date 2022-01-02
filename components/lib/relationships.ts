278fe9248b50323434587136fb11957cb7fd4a34
Calum Mackervoy <c.mackervoy@gmail.com>
2022-01-02 16:06:33 +0000

    tarot card assets

--- components/lib/relationships.ts
+++ components/lib/relationships.ts
@@ -17,3 +17,24 @@ export enum SelfIdentityLabels {
     REVOLUTIONARY="revolutionary",
     RUTHLESS="ruthless"
 }
+
+export enum TarotCardNames {
+    COURAGE="courage",
+    GREENSIGHT="greensight",
+}
+
+export interface ITarotCard {
+    name: string;
+    imgSrc: string;
+}
+
+export const tarotCards: {[key: string]: ITarotCard} = {
+    [TarotCardNames.COURAGE]: {
+        name: TarotCardNames.COURAGE,
+        imgSrc: "../../../public/img/tarot/courage.jpg"
+    },
+    [TarotCardNames.GREENSIGHT]: {
+        name: TarotCardNames.GREENSIGHT,
+        imgSrc: "../../../public/img/tarot/greensight.jpg"
+    },
+}
