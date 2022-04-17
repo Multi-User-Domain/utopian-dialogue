e57b0a2d57f4213833d6a6a80d6382d681e6f8cf
Calum Mackervoy <c.mackervoy@gmail.com>
2022-04-17 20:05:28 +0200

    fix FAST_PACE constant

--- components/lib/constants.ts
+++ components/lib/constants.ts
@@ -1,6 +1,6 @@
 export const LONG_PAUSE = 2000;
 export const SHORT_PAUSE = 1000;
-export const SLOW_PACE = 100;
-export const FAST_PACE = 1000;
+export const SLOW_PACE = 150;
+export const FAST_PACE = 40;
 
 export const INTUITION_COLOUR = "#9246d9"
