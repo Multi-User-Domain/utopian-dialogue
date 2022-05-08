3f056a081fecb35c398b2dd75ce7f3e50007b065
Calum Mackervoy <c.mackervoy@gmail.com>
2022-05-08 21:23:14 +0200

    feature (DevTools): view world state in tab

--- components/lib/devTools/index.tsx
+++ components/lib/devTools/index.tsx
@@ -7,14 +7,22 @@ import {
     DrawerBody,
     DrawerOverlay,
     DrawerCloseButton,
-    DrawerContent
+    DrawerContent,
+    Tabs,
+    TabList, 
+    TabPanels, 
+    Tab, 
+    TabPanel, 
+    Text
 } from "@chakra-ui/react";
 
 import { FRAME_DICTIONARY } from "../../../context/narrativeContext";
 import useNarrative from "../../../hooks/useNarrative";
+import useBigCity from "../../../hooks/useBigCity";
 
 export default function DevTools({isOpen, onClose=null} : {isOpen: boolean, onClose: () => void}): React.ReactElement {
     const { setFrame } = useNarrative();
+    const { world } = useBigCity();
     
     const goToFrame = (idx: string) => {
         setFrame(idx);
@@ -26,14 +34,38 @@ export default function DevTools({isOpen, onClose=null} : {isOpen: boolean, onCl
         frameList.push(<Container key={name} w="100%" marginBottom={3}><Button colorScheme="teal" onClick={() => goToFrame(name)}>{name}</Button></Container>)
     }
 
+    // render world data into a JSON-object like display
+    const worldData = [<Text key={0}>&#123;</Text>];
+    Object.keys(world).forEach((key) => {
+        worldData.push(<Text key={key} paddingLeft="2em">{key}: {world[key]}</Text>);
+    });
+    worldData.push(<Text key={Object.keys(world).length}>&#125;</Text>);
+
+    const worldDisplay = worldData.length > 2 ? worldData : [];
+
     return (
-        <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="sm">
+        <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="lg">
             <DrawerOverlay>
                 <DrawerContent>
                     <DrawerCloseButton />
 
                     <DrawerBody paddingTop={10}>
-                        {frameList}
+                    <Tabs isFitted>
+                        <TabList>
+                            <Tab>Navigation</Tab>
+                            <Tab>Game State</Tab>
+                        </TabList>
+                        
+                        <TabPanels>
+                            <TabPanel>
+                                {frameList}
+                            </TabPanel>
+                            <TabPanel>
+                                <Text fontSize='xl'>{world.name}</Text>
+                                {worldDisplay}
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
                     </DrawerBody>
                 </DrawerContent>
             </DrawerOverlay>
