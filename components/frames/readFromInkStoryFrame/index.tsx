fcdc85bd744914de688568401b7a76a8d6e30234
Calum Mackervoy <c.mackervoy@gmail.com>
2022-08-13 14:46:48 +0200

    feature: URL for ink story read from user input

--- components/frames/readFromInkStoryFrame/index.tsx
+++ components/frames/readFromInkStoryFrame/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import InkJs from 'inkjs';
 import axios from 'axios';
-import { Text } from "@chakra-ui/react";
+import { Text, Container, Button, Center, Input } from "@chakra-ui/react";
 
 import { IStoryFrame } from "../../lib/types";
 import Dialogue from "../../../components/lib/dialogue";
@@ -9,7 +9,7 @@ import useDialogue from "../../../hooks/useDialogue";
 import usePlayer from "../../../hooks/usePlayer";
 import { IMessage, DialogueProvider } from "../../../context/dialogueContext";
 
-import { performers, PerformerNames, IPerformer } from "../../lib/performers";
+import { performers, PerformerNames } from "../../lib/performers";
 
 /*
 *   A component which renders a dialogue directly from an ink file
@@ -18,9 +18,9 @@ import { performers, PerformerNames, IPerformer } from "../../lib/performers";
 function ReadFromInkDialogue({followLink} : IStoryFrame) : React.ReactElement {
     const { addMessage, dialogueEnded, setDialogueEnded, selectRandomFrom } = useDialogue();
     const { playerPerformer } = usePlayer();
+    const [storyUrl, setStoryUrl] = useState(null);
+    const [storyUrlInput, setStoryUrlInput] = useState("https://calum.inrupt.net/public/utopian-dialogue/achilles.ink.json");
     const [inkStory, setInkStory] = useState(null);
-
-    const storyUrl = "https://calum.inrupt.net/public/utopian-dialogue/achilles.ink.json";
 
     const getPerformerFromContent = (content: string) => {
         if(content.startsWith("<") && content.indexOf(":>") > 0) {
@@ -86,14 +86,33 @@ function ReadFromInkDialogue({followLink} : IStoryFrame) : React.ReactElement {
     }
 
     useEffect(() => {
-        axios.get(storyUrl).then(res => {
-            setInkStory(new InkJs.Story(res.data));
-        });
-    }, []);
+        if(storyUrl != null) {
+            axios.get(storyUrl).then(res => {
+                setInkStory(new InkJs.Story(res.data));
+            });
+        }
+    }, [storyUrl]);
 
     useEffect(() => {
         if(inkStory != null) getNext();
     }, [inkStory]);
+
+    if(storyUrl == null) {
+        return (
+            <Container>
+                <Center marginTop={15}>
+                    <Input
+                        label="Story URL"
+                        type="url"
+                        value={storyUrlInput}
+                        onChange={(e) => setStoryUrlInput(e.target.value)}/>
+                </Center>
+                <Center marginTop={10}>
+                    <Button onClick={() => setStoryUrl(storyUrlInput)}>Submit</Button>
+                </Center>
+            </Container>
+        );
+    }
 
     return (
         <Dialogue>
---
+++ ink/achilles.ink
@@ -0,0 +1,20 @@
+
+->introduce_achilles
+
+=== introduce_achilles ===
+    <Craig:> "Greetings! I am Achilles, Champion of all the Greeks! And who are <em>you</em>, strange humanoid?"
+    
+    <Rupert:> "And <em>I</em> am King Rupert"
+    
+    <Craig:> "Nobody cares who you are"
+    
+    * <Player:> I am Player One, number one of all players -> champion
+    * <Player:> Greetitudes! I am Player One -> not_champion
+    
+== champion ==
+    <Craig:> "Champion, eh?". Achilles seems impressed
+    -> DONE
+
+== not_champion ==
+    <Craig:> "Ha! Just Player One. Champion of nothing and superior to nobody"
+    -> DONE
