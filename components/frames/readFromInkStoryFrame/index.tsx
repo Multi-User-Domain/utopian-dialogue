3df69543353196fa858926ab2ace4e3abb41f0ec
Calum Mackervoy <c.mackervoy@gmail.com>
2022-08-13 13:08:02 +0200

    feature: reading ink JSON from hardcoded URL

--- components/frames/readFromInkStoryFrame/index.tsx
+++ components/frames/readFromInkStoryFrame/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import InkJs from 'inkjs';
+import axios from 'axios';
 import { Text } from "@chakra-ui/react";
 
 import { IStoryFrame } from "../../lib/types";
@@ -17,9 +18,9 @@ import { performers, PerformerNames, IPerformer } from "../../lib/performers";
 function ReadFromInkDialogue({followLink} : IStoryFrame) : React.ReactElement {
     const { addMessage, dialogueEnded, setDialogueEnded, selectRandomFrom } = useDialogue();
     const { playerPerformer } = usePlayer();
+    const [inkStory, setInkStory] = useState(null);
 
-    const storyJson = {"inkVersion":20,"root":[[{"->":"introduce_achilles"},["done",{"#f":5,"#n":"g-0"}],null],"done",{"introduce_achilles":[["^<Craig:> \"Greetings! I am Achilles, Champion of all the Greeks! And who are <em>you</em>, strange humanoid?\"","\n","^<Rupert:> \"And <em>I</em> am King Rupert\"","\n","^<Craig:> \"Nobody cares who you are\"","\n",["ev",{"^->":"introduce_achilles.0.6.$r1"},{"temp=":"$r"},"str",{"->":".^.s"},[{"#n":"$r1"}],"/str","/ev",{"*":".^.^.c-0","flg":18},{"s":["^<Player:> I am Player One, number one of all players ",{"->":"$r","var":true},null]}],["ev",{"^->":"introduce_achilles.0.7.$r1"},{"temp=":"$r"},"str",{"->":".^.s"},[{"#n":"$r1"}],"/str","/ev",{"*":".^.^.c-1","flg":18},{"s":["^<Player:> Greetitudes! I am Player One ",{"->":"$r","var":true},null]}],{"c-0":["ev",{"^->":"introduce_achilles.0.c-0.$r2"},"/ev",{"temp=":"$r"},{"->":".^.^.6.s"},[{"#n":"$r2"}],{"->":"champion"},"\n",{"#f":5}],"c-1":["ev",{"^->":"introduce_achilles.0.c-1.$r2"},"/ev",{"temp=":"$r"},{"->":".^.^.7.s"},[{"#n":"$r2"}],{"->":"not_champion"},"\n",{"#f":5}]}],{"#f":1}],"champion":["^<Craig:> \"Champion, eh?\". Achilles seems impressed","\n","done",{"#f":1}],"not_champion":["^<Craig:> \"Ha! Just Player One. Champion of nothing and superior to nobody\"","\n","done",{"#f":1}],"#f":1}],"listDefs":{}}
-    const inkStory = new InkJs.Story(storyJson);
+    const storyUrl = "https://calum.inrupt.net/public/utopian-dialogue/achilles.ink.json";
 
     const getPerformerFromContent = (content: string) => {
         if(content.startsWith("<") && content.indexOf(":>") > 0) {
@@ -85,8 +86,14 @@ function ReadFromInkDialogue({followLink} : IStoryFrame) : React.ReactElement {
     }
 
     useEffect(() => {
-        getNext();
+        axios.get(storyUrl).then(res => {
+            setInkStory(new InkJs.Story(res.data));
+        });
     }, []);
+
+    useEffect(() => {
+        if(inkStory != null) getNext();
+    }, [inkStory]);
 
     return (
         <Dialogue>
