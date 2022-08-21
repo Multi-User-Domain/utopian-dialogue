b4779cb6cf07007487a750f5acd9e5d0dfd79e23
Calum Mackervoy <c.mackervoy@gmail.com>
2022-08-21 18:36:30 +0200

    feature: automated pacing of '...' in text

--- components/frames/readFromInkStoryFrame/index.tsx
+++ components/frames/readFromInkStoryFrame/index.tsx
@@ -11,13 +11,14 @@ import usePlayer from "../../../hooks/usePlayer";
 import { IMessage, DialogueProvider } from "../../../context/dialogueContext";
 
 import { performers, PerformerNames } from "../../lib/performers";
+import { LONG_PAUSE, SHORT_PAUSE, SLOW_PACE, INTUITION_COLOUR, FAST_PACE } from "../../lib/constants";
 
 /*
 *   A component which renders a dialogue directly from an ink file
 */
 
 function ReadFromInkDialogue({followLink} : IStoryFrame) : React.ReactElement {
-    const { addMessage, dialogueEnded, setDialogueEnded, selectRandomFrom } = useDialogue();
+    const { addMessage } = useDialogue();
     const { playerPerformer } = usePlayer();
     const [storyUrl, setStoryUrl] = useState(null);
     const [storyUrlInput, setStoryUrlInput] = useState("https://calum.inrupt.net/public/utopian-dialogue/achilles.ink.json");
@@ -80,6 +81,7 @@ function ReadFromInkDialogue({followLink} : IStoryFrame) : React.ReactElement {
         s = stripPerformerFromContent(s);
         let contentArr = [];
 
+        // first find all the explicit examples of animation controls
         let index = 0;
         while(index >= 0) {
             index = s.indexOf("<");
@@ -94,17 +96,27 @@ function ReadFromInkDialogue({followLink} : IStoryFrame) : React.ReactElement {
             let elementName = element.shift();
 
             // find the element and push it to the contentArr
-            contentArr.push(<span key={index}>{s.substring(0, index)}</span>);
+            contentArr.push(<span key={contentArr.length}>{s.substring(0, index)}</span>);
             if(Object.keys(customInkElementDict).includes(elementName)) {
-                contentArr.push(customInkElementDict[elementName](index + 1, ...element));
+                contentArr.push(customInkElementDict[elementName](contentArr.length, ...element));
             }
 
             // strip the element from the content
             s = s.substring(end, s.length);
         }
 
+        // find all automated animations
+        index = 0;
+        while(index >= 0) {
+            index = s.indexOf("...");
+            if(index < 0) break;
+
+            s = s.substring(0, index) + s.substring(index + 3, s.length);
+            contentArr.push(<span key={contentArr.length}><Pace ms={SLOW_PACE}>...</Pace></span>)
+        }
+
         // build the content into a ReactElement
-        contentArr.push(<span key={s.length}>{s}</span>);
+        contentArr.push(<span key={contentArr.length}>{s}</span>);
         let content = <Text>{contentArr}</Text>;
 
         return content;
